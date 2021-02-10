1aec4001ccba49a4c6cba9d5cc761d144fc54e92
Ethan O'Brien <ethan.a.obrien@gmail.com>
2021-02-10 15:33:49 -0600

    Update Create Qemu Image.bat

--- Create Qemu Image.bat
+++ Create Qemu Image.bat
@@ -1,4 +1,4 @@
 @echo off
 set /P NAME="What do you want to name the disk "
 set /P SIZE="How big would you like the disk to be? (In Gb) "
-qemu-img create -f raw %USERPROFILE%\Desktop\%NAME%.img %SIZE%G
+qemu\qemu-img create -f raw %USERPROFILE%\Desktop\%NAME%.img %SIZE%G
