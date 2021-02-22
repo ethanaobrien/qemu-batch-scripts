1e6fc497723ec6a7c4524d2c075b08a72c223f5d
Ethan O'Brien <ethan.a.obrien@gmail.com>
2021-02-22 10:28:20 -0600

    Update x86_64.bat
    added multi core support

--- x86_64.bat
+++ x86_64.bat
@@ -3,7 +3,7 @@ echo 'This script is for x86_64 systems (64 or 32 bit)'
 echo 'Only raw images (.raw .iso .img) can be loaded using this script'
 set /P NAME='What is the name of the HARD DISK? (iso disk next) INCLUDE THE FILE TYPE! Example: .img (It MUST be on your desktop!!)'
 set /P RAM='How much ram do you want to give the system in MiB? (512 recommended)'
-
+set /P SMP='How many virtual processers?'
 
 :start
 SET choice=
@@ -21,10 +21,10 @@ GOTO start
 
 :yes
 set /P ISONAME='What is the name of the iso disk? INCLUDE THE FILE TYPE! Example: .iso (It MUST be on your desktop!!) THIS DISK WILL BE READ ONLY!!'
-%USERPROFILE%\desktop\qemu\qemu-system-x86_64.exe -m %RAM% -drive format=raw,file=%USERPROFILE%\desktop\%NAME% -drive format=raw,media=cdrom,readonly,file=%USERPROFILE%\desktop\%ISONAME%
+%USERPROFILE%\desktop\qemu\qemu-system-x86_64.exe -m %RAM% -smp %SMP% -drive format=raw,file=%USERPROFILE%\desktop\%NAME% -drive format=raw,media=cdrom,readonly,file=%USERPROFILE%\desktop\%ISONAME%
 
 
 
 
 :no
-%USERPROFILE%\desktop\qemu\qemu-system-x86_64.exe -m %RAM% -drive format=raw,file=%USERPROFILE%\desktop\%NAME%
+%USERPROFILE%\desktop\qemu\qemu-system-x86_64.exe -m %RAM% -smp %SMP% -drive format=raw,file=%USERPROFILE%\desktop\%NAME%
